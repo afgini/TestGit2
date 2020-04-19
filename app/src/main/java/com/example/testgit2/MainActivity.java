4e39cd8d26ace0ad9166914f9d18ff5c70b60850
HP <afgini@gmail.com>
2020-04-19 22:16:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testgit2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.testgit2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "TestGit2", Toast.LENGTH_SHORT).show();
+    }
+}
